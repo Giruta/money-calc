c08a936435688c4c26247a8985c1905ec1b5259c
Irina Giruta <irina.giruta@gmail.com>
2020-08-17 17:19:53 +0300

    Updates

---
+++ precache-manifest.21f9575992de07889ae9523c2f745d8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf3fc41b19dcd5fd67b98e403f9db8b",
+    "url": "/calc-money/index.html"
+  },
+  {
+    "revision": "a97ba3285ff9c78d40c6",
+    "url": "/calc-money/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "ab12b72ec6cd5010e57a",
+    "url": "/calc-money/static/js/2.42df7b18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calc-money/static/js/2.42df7b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97ba3285ff9c78d40c6",
+    "url": "/calc-money/static/js/main.8fd17282.chunk.js"
+  },
+  {
+    "revision": "7f94107833afc5735ccc",
+    "url": "/calc-money/static/js/runtime-main.b19a42aa.js"
+  }
+]);
